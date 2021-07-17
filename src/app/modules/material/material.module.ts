799f62774f063a5ea85d7d53f94a8846b9b597cc
Nihad KV <nihad083@gmail.com>
2021-07-17 23:19:14 +0530

    toolbar implemented

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBadgeModule],
+})
+export class MaterialModule {}
